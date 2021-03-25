6b58b2aa67ac7c4e1d437f0c4b7227fb681a79b6
Daniel Hentosz <57598807+danhentosz@users.noreply.github.com>
2021-03-25 13:51:55 -0400

    Added the waitmsg irvine32 procedure call to the showjobs PROC

--- G10P3.asm
+++ G10P3.asm
@@ -7,8 +7,8 @@ INCLUDE irvine32.inc
 input byte 48 dup(? )
 spot dword ?
 command byte 8 dup(? )
-op1 byte 10 dup(? ) 
-op2 sbyte -1
+op1 byte 10 dup(? )
+op2 sbyte - 1
 op3 byte ?
 ;//___________________
 msg1 byte "Command: ", 0
@@ -17,10 +17,10 @@ msg3 byte "Priority: ", 0
 msg4 byte "Run Time: ", 0
 .code
 main PROC
-mov eax,black+(gray*16)
+mov eax, black + (gray * 16)
 call settextcolor
 call clrscr
-beginit:
+beginit :
 mov edx, offset input
 mov ecx, 48
 call readstring
@@ -36,162 +36,162 @@ exit
 main ENDP
 
 rem PROC
-mov esi,spot
-mov ecx,48
+mov esi, spot
+mov ecx, 48
 L1:
-mov al,byte ptr [esi]
+mov al, byte ptr[esi]
 inc esi
-cmp al,' '
+cmp al, ' '
 loope L1
-cmp al,tab
+cmp al, tab
 loope L1
 dec esi
-mov spot,esi
+mov spot, esi
 ret
 rem ENDP
 
 skipchars PROC
-mov esi,spot
-mov ecx,48
+mov esi, spot
+mov ecx, 48
 L1:
-mov al,byte ptr [esi]
-cmp al,' '
+mov al, byte ptr[esi]
+cmp al, ' '
 je done
-cmp al,tab
+cmp al, tab
 je done
-cmp al,null
+cmp al, null
 je done
 inc esi
 loop L1
-done:
-mov spot,esi
+done :
+mov spot, esi
 ret
 skipchars ENDP
 
 getcommand PROC
-mov esi,spot
-mov edi,offset command
-mov ecx,7
+mov esi, spot
+mov edi, offset command
+mov ecx, 7
 L1:
-mov al,byte ptr [esi]
-cmp al,' '
+mov al, byte ptr[esi]
+cmp al, ' '
 je done
-cmp al,tab
+cmp al, tab
 je done
-cmp al,null
+cmp al, null
 je done
 cld
 movsb
 loop L1
-done:
-mov spot,esi
+done :
+mov spot, esi
 ret
 getcommand ENDP
 
 getop1 PROC
-mov esi,spot
-mov edi,offset op1
-mov ecx,9
+mov esi, spot
+mov edi, offset op1
+mov ecx, 9
 L1:
-mov al,byte ptr [esi]
-cmp al,' '
+mov al, byte ptr[esi]
+cmp al, ' '
 je done
-cmp al,tab
+cmp al, tab
 je done
-cmp al,null
+cmp al, null
 je done
 cld
 movsb
 inc namelen
 loop L1
-done:
-mov spot,esi
+done :
+mov spot, esi
 ret
 getop1 ENDP
 
 getop2 PROC
-mov esi,spot
+mov esi, spot
 inc esi
-mov al,byte ptr [esi]
+mov al, byte ptr[esi]
 call isdigit
 jz bad
-cmp al,' '
+cmp al, ' '
 je good1
-cmp al,tab
+cmp al, tab
 je good1
-cmp al,null
+cmp al, null
 je good1
 jmp bad
-good1:
+good1 :
 dec esi
 mov al, byte ptr[esi]
 call isdigit
 jz good2
 jmp bad
-good2:
+good2 :
 mov edx, esi
 mov ecx, 1
 call parsedecimal32
-cmp al,7
+cmp al, 7
 jg bad
-cmp al,0
+cmp al, 0
 jl bad
-mov op2,al
+mov op2, al
 jmp done
-bad:
-mov op2,-1
-done:
-	mov spot,esi
-ret
-getop2 ENDP
+bad :
+mov op2, -1
+done :
+	mov spot, esi
+	ret
+	getop2 ENDP
 
-getop3 PROC
-mov esi,spot
-mov al,byte ptr [esi]
-call isdigit
-je good1
-jmp bad
-good1:
-mov esi,spot
+	getop3 PROC
+	mov esi, spot
+	mov al, byte ptr[esi]
+	call isdigit
+	je good1
+	jmp bad
+	good1 :
+mov esi, spot
 inc esi
-mov al,byte ptr [esi]
+mov al, byte ptr[esi]
 call isdigit
 jz good2dig
-cmp al,' '
+cmp al, ' '
 je good1dig
-cmp al,tab
+cmp al, tab
 je good1dig
-cmp al,null
+cmp al, null
 je good1dig
 jmp bad
-good2dig:
-mov esi,spot
-mov al,byte ptr [esi]
-and al,00001111b
-mov dl,10
+good2dig :
+mov esi, spot
+mov al, byte ptr[esi]
+and al, 00001111b
+mov dl, 10
 mul dl
 mov op3, al
 inc esi
-mov al,byte ptr [esi]
-and al,00001111b
-add op3,al
+mov al, byte ptr[esi]
+and al, 00001111b
+add op3, al
 jmp done
-good1dig:
-mov esi,spot
-mov al,byte ptr [esi]
-and al,00001111b
-mov op3,al
+good1dig :
+mov esi, spot
+mov al, byte ptr[esi]
+and al, 00001111b
+mov op3, al
 jmp done
-bad:
-mov op3,null
+bad :
+mov op3, null
 jmp done2
-done:
-mov al,op3
-cmp al,50
+done :
+mov al, op3
+cmp al, 50
 ja bad
-cmp al,1
+cmp al, 1
 jb bad
-done2:
+done2 :
 ret
 getop3 ENDP
 
@@ -252,7 +252,7 @@ compare ENDP
 
 change PROC
 .data
-foundchange byte "Job Priority Changed.",0dh,0ah,0
+foundchange byte "Job Priority Changed.", 0dh, 0ah, 0
 .code
 call skipchars
 call rem
@@ -291,26 +291,26 @@ mov al, op2
 cmp al, 8
 jae priprompt
 jmp cont
-priprompt:
+priprompt :
 call getnewpri
 mov al, op2
 cmp al, 7
 ja done
-cont:
+cont :
 ;//Now just get the new priority, check it, and place it
 ;//jobnamepos is pointing to the start of the record
 mov esi, jobnamepos
 add esi, 9
 movzx eax, op2
-mov byte ptr[esi],al
-mov edx,offset foundchange
+mov byte ptr[esi], al
+mov edx, offset foundchange
 call writestring
-done:
+done :
 ret
 change ENDP
 kill PROC
 .data
-foundkill byte "Job Killed.",0dh,0ah,0
+foundkill byte "Job Killed.", 0dh, 0ah, 0
 .code
 call skipchars
 call rem
@@ -340,13 +340,13 @@ call findjob
 mov al, jobnamenum
 cmp al, 0
 je done
-mov esi,jobnamepos
-add esi,10
-mov al,byte ptr [esi]
-cmp al,2
+mov esi, jobnamepos
+add esi, 10
+mov al, byte ptr[esi]
+cmp al, 2
 je good
 jmp bad
-good:
+good :
 mov edx, offset foundkill
 call writestring
 ;//Call a kill procedure here.
@@ -360,10 +360,10 @@ mov ecx, 14
 rep movsb
 ;//___________________________________________________
 jmp done
-bad:
-mov edx,offset badstatusmsg
+bad :
+mov edx, offset badstatusmsg
 call writestring
-done:
+done :
 ret
 kill ENDP
 
@@ -407,8 +407,8 @@ hold ENDP
 
 run PROC
 .data
-notfoundrun byte "Job doesn't exist.",0dh,0ah,0
-foundrun byte "Job Status Updated.",0dh,0ah,0
+notfoundrun byte "Job doesn't exist.", 0dh, 0ah, 0
+foundrun byte "Job Status Updated.", 0dh, 0ah, 0
 .code
 call skipchars
 call rem
@@ -420,7 +420,7 @@ je nameprompt
 cmp al, 9
 jae nameprompt
 jmp keepgoing
-nameprompt:
+nameprompt :
 mov namelen, 0
 cld
 mov edi, offset op1
@@ -433,65 +433,65 @@ cmp al, 0
 jbe done
 cmp al, 9
 jae done
-keepgoing:
+keepgoing :
 call findjob
-mov al,jobnamenum
-cmp al,0
+mov al, jobnamenum
+cmp al, 0
 je done
-mov edx,offset foundrun
+mov edx, offset foundrun
 call writestring
-mov edi,jobnamepos
-add edi,10
-mov byte ptr [edi],1
+mov edi, jobnamepos
+add edi, 10
+mov byte ptr[edi], 1
 done:
 ret
 run ENDP
 
 findjob PROC
 .data
-jobnotfoundmsg byte "Job not found.",0dh,0ah,0
+jobnotfoundmsg byte "Job not found.", 0dh, 0ah, 0
 jobnamepos dword ?
 jobnamenum byte 9
 .code
-mov jobnamenum,10
-mov jobnamepos,offset jobs
-again:
-mov al,jobnamenum
-cmp al,0
+mov jobnamenum, 10
+mov jobnamepos, offset jobs
+again :
+mov al, jobnamenum
+cmp al, 0
 je nope
 cld
-mov esi,jobnamepos
-mov edi,offset op1
-mov ecx,2
+mov esi, jobnamepos
+mov edi, offset op1
+mov ecx, 2
 repe cmpsd
 je found
-add jobnamepos,14
+add jobnamepos, 14
 dec jobnamenum
 jmp again
-nope:
-mov edx,offset jobnotfoundmsg
+nope :
+mov edx, offset jobnotfoundmsg
 call writestring
 jmp done
-found:
+found :
 done:
 ret
 findjob ENDP
 
 load PROC
 .data
-jobs byte 140 dup(?)
+jobs byte 140 dup(? )
 jobsfull dword jobsfull
 curjob dword jobs
 totjobs byte 0
-fullmsg byte "Job Record Full",0dh,0ah,0
+fullmsg byte "Job Record Full", 0dh, 0ah, 0
 jobava equ 0
 jobrun equ 1
 jobhold equ 2
 .code
 cld
-mov esi,curjob
-mov edi,jobsfull
-mov ecx,2
+mov esi, curjob
+mov edi, jobsfull
+mov ecx, 2
 repe cmpsd
 je full
 call skipchars
@@ -506,22 +506,22 @@ call rem
 call getop3
 ;//Compare the run time here
 ;//Compare the name here
-mov al,namelen
-cmp al,0
+mov al, namelen
+cmp al, 0
 je nameprompt
-cmp al,9
+cmp al, 9
 jae nameprompt
 call dupname
-mov al,dupnamecount
-cmp al,0
+mov al, dupnamecount
+cmp al, 0
 jne done
 jmp cont
-nameprompt:
-mov namelen,0
+nameprompt :
+mov namelen, 0
 cld
-mov edi,offset op1
-mov esi,offset empty
-mov ecx,10
+mov edi, offset op1
+mov esi, offset empty
+mov ecx, 10
 rep movsb
 call getnewname
 mov al, namelen
@@ -533,84 +533,84 @@ call dupname
 mov al, dupnamecount
 cmp al, 0
 jne done
-cont:
-mov al,op2
-cmp al,8
+cont :
+mov al, op2
+cmp al, 8
 jae priprompt
-jmp cont2 
-priprompt:
+jmp cont2
+priprompt :
 call getnewpri
-mov al,op2
-cmp al,7
+mov al, op2
+cmp al, 7
 ja done
-cont2:
-mov al,op3
-cmp al,50
+cont2 :
+mov al, op3
+cmp al, 50
 ja runprompt
-cmp al,0
+cmp al, 0
 jbe runprompt
 jmp cont3
-runprompt:
+runprompt :
 call getnewrun
-mov al,op3
-cmp al,50
+mov al, op3
+cmp al, 50
 ja done
-cmp al,0
+cmp al, 0
 jbe done
-cont3:
+cont3 :
 inc totjobs
 cld
-mov esi,offset op1
-mov edi,curjob
-mov ecx,8
+mov esi, offset op1
+mov edi, curjob
+mov ecx, 8
 rep movsb
-mov edi,curjob
-add edi,9
-movzx eax,op2
-mov byte ptr [edi],al
-mov edi,curjob
-add edi,10
-mov byte ptr [edi],2
-mov edi,curjob
-add edi,11
-movzx eax,op3
-mov byte ptr [edi],al
-add curjob,14
+mov edi, curjob
+add edi, 9
+movzx eax, op2
+mov byte ptr[edi], al
+mov edi, curjob
+add edi, 10
+mov byte ptr[edi], 2
+mov edi, curjob
+add edi, 11
+movzx eax, op3
+mov byte ptr[edi], al
+add curjob, 14
 jmp done
-full:
-mov edx,offset fullmsg
+full :
+mov edx, offset fullmsg
 call writestring
-done:
+done :
 ret
 load ENDP
 
 dupname PROC
 .data
 dupnamecount byte 10
-dupnamemsg byte "Job Name Already Exists.",0dh,0ah,0
+dupnamemsg byte "Job Name Already Exists.", 0dh, 0ah, 0
 currentpostemp dword jobs
 .code
-mov currentpostemp,offset jobs
-mov edi,currentpostemp
-mov esi,offset op1
-mov dupnamecount,10
+mov currentpostemp, offset jobs
+mov edi, currentpostemp
+mov esi, offset op1
+mov dupnamecount, 10
 again:
-mov al,dupnamecount
-cmp al,0
+mov al, dupnamecount
+cmp al, 0
 je done
 mov esi, offset op1
 cld
-mov ecx,2
+mov ecx, 2
 repe cmpsd
 jz bad
-add currentpostemp,14
-mov edi,currentpostemp
+add currentpostemp, 14
+mov edi, currentpostemp
 dec dupnamecount
 jmp again
-bad:
-mov edx,offset dupnamemsg
+bad :
+mov edx, offset dupnamemsg
 call writestring
-done:
+done :
 ret
 dupname ENDP
 getnewrun PROC
@@ -660,7 +660,6 @@ ret
 getnewname ENDP
 showjobs PROC
 .data
-totaljobs byte 1
 tempspot byte 2 dup(? )
 showname byte "Name: ", 0
 showpri byte "Priority: ", 0
@@ -670,22 +669,24 @@ showloadtime byte "Load Time: ", 0dh, 0ah, 0
 jobnummsg2 byte "Info:", 0dh, 0ah, 0
 jmsg1 byte " ", 0dh, 0ah, 0dh, 0ah, 0
 priposition dword ?
+pausecount byte 0
 .code
-mov ecx,10
-mov priposition,offset jobs
-mov esi,priposition
-again:
-add esi,10
-mov al,byte ptr [esi]
-cmp al,null
+mov pausecount,0
+mov ecx, 10
+mov priposition, offset jobs
+mov esi, priposition
+again :
+add esi, 10
+mov al, byte ptr[esi]
+cmp al, null
 jne good
 add priposition, 14
 mov esi, priposition
 loop again
 jmp done
-good:
+good :
 call printnameneat
-mov edx,offset showpri
+mov edx, offset showpri
 call writestring
 dec esi
 movzx eax, byte ptr[esi]
@@ -701,18 +702,36 @@ inc esi
 movzx eax, byte ptr[esi]
 call writedec
 call crlf
-mov edx, offset showloadtime
-call writestring
-call crlf
-add priposition,14
-mov esi,priposition
+call showlt
+add priposition, 14
+mov esi, priposition
 loop again
-done:
+done :
 ret
 showjobs ENDP
 
+showlt PROC
+mov edx, offset showloadtime
+call writestring
+mov al,pausecount
+cmp al,30
+je cont
+call crlf
+cont:
+ret
+showlt ENDP
+maybewait PROC
+ret
+maybewait ENDP
 printnameneat PROC
-mov edx,offset showname
+add pausecount,6
+mov al,pausecount
+cmp al,36
+jne cont
+call waitmsg
+call crlf
+cont:
+mov edx, offset showname
 call writestring
 mov edx, priposition
 call writestring
@@ -720,32 +739,32 @@ call crlf
 ret
 printnameneat ENDP
 disppri PROC uses esi
-mov esi,priposition
-mov al,byte ptr [esi+10]
-cmp al,2
+mov esi, priposition
+mov al, byte ptr[esi + 10]
+cmp al, 2
 je theholding
 jmp therunning
-theholding:
-mov edx,offset holdstr
+theholding :
+mov edx, offset holdstr
 call writestring
 call crlf
 jmp done
-therunning:
+therunning :
 mov edx, offset runstr
 call writestring
 call crlf
-done:
+done :
 ret
 disppri ENDP
 initstuff PROC
 .data
-empty byte 48 dup(?)
+empty byte 48 dup(? )
 .code
-mov namelen,0
+mov namelen, 0
 cld
-mov edi,offset command
-mov esi,offset empty
-mov ecx,8
+mov edi, offset command
+mov esi, offset empty
+mov ecx, 8
 rep movsb
 
 cld
@@ -754,13 +773,13 @@ mov esi, offset empty
 mov ecx, 10
 rep movsb
 
-mov op2,-1
-mov op3,null
+mov op2, -1
+mov op3, null
 
 cld
 mov edi, offset input
 mov esi, offset empty
-mov ecx,48
+mov ecx, 48
 rep movsb
 ret
 initstuff ENDP
