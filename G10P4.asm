1bc3dc778079b0e253469ae548f712987292234b
Daniel Hentosz <57598807+danhentosz@users.noreply.github.com>
2021-04-10 16:57:47 -0400

    Template

---
+++ G10P4.asm
@@ -0,0 +1,14 @@
+;//G10P4 || Assembly Language Programming CSC 323
+;//Daniel Hentosz, Scott Trunzo, Josh Staffen || GROUP 10
+;//hen3883@calu.edu, tru1931@calu.edu, sta9036@calu.edu
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+INCLUDE irvine32.inc
+.data
+.code
+main PROC
+exit
+main ENDP
+END main
